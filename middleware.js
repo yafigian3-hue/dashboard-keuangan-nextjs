3e055b101b6025a0345f94666d5b852f3a00cbfb
yafi <yafigian3@gmail.com>
2026-08-07 22:44:25 -0700

    feat: complete authentication and transaction API

--- middleware.js
+++ middleware.js
@@ -1,10 +1,7 @@
 import { NextResponse } from "next/server";
-import { verifyToken } from "@/lib/jwt";
 
 export function middleware(request) {
   const token = request.cookies.get("token")?.value;
-
-  console.log("TOKEN :", token);
 
   return NextResponse.next();
 }
